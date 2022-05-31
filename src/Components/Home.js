2ef549f96e1d06cf645dcd3fe165c9e7d5bcf525
Rahil32 <rahiltopno32@gmail.com>
2022-05-31 14:54:14 +0530

    puff

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white border-0">
+  <img src="/assets/puff.jpg" className="card-img" alt="Background" height="550px"/>
+  <div className="card-img-overlay d-flex flex-column justify-content-center">
+    <h5 className="card-title display-2 fw-italic mb-0 text-start">Trend Setter</h5>
+    <p className="card-text lead fs-2 text-start ">Your own style is the trend</p>
+  </div>
+  
+</div>
+    </div>
+  )
+}
+
+export default Home
+
+
